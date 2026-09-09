dadb932d3d6b05a082c1fa98a21eddeecd5ee6ce
Minvydas Lazauskas <minderrx2@gmail.com>
2026-09-09 21:34:26 +0100

    Add: python-loop-unpacking - submission-0

---
+++ Python Coding Interviews/python-loop-unpacking/submission-0.py
@@ -0,0 +1,18 @@
+from typing import List, SupportsComplex, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    highest = float("-inf")
+    highestName = ""
+    for name,score in scores:
+        if score > highest:
+            highest = score
+            highestName = name
+    return highestName
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
